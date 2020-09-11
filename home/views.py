7f1f3ef5dcf03f9cebab0c1990d6916347c55554
Ricky Rodriguez <ricardo.rodriguez4@yahoo.com>
2020-09-11 12:37:30 -0500

    new branch

--- home/views.py
+++ home/views.py
@@ -1,5 +1,8 @@
-from django.shortcuts import render, redirect
-from .models import User, Taco, Order
+from django.shortcuts import render, redirect, HttpResponse, HttpResponseRedirect
+from django.contrib.auth.models import User
+from django.contrib.auth.decorators import login_required
+from django.contrib.admin.views.decorators import staff_member_required
+from .models import *
 from django.contrib import messages
 
 # Create your views here.
