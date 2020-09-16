4b9e27d4561568079b24e8ef506fb290b9910a1f
Ricky Rodriguez <ricardo.rodriguez4@yahoo.com>
2020-09-16 15:46:57 -0500

    Update to models must debug

--- home/models.py
+++ home/models.py
@@ -9,16 +9,8 @@ import re
 EMAIL_MATCH = re.compile(r'^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]+$')
 
 
-class Taco(models.Model):
-    taco_type = models.CharField(max_length=255)
-    price = models.DecimalField(decimal_places=2, max_digits=5)
-    tacos_for = models.ForeignKey(User, related_name='taco_history', on_delete=models.CASCADE)
-    favorite = models.ForeignKey(User, related_name='favorite_taco', on_delete=models.CASCADE, default = None)
-
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
-
 class Order(models.Model):
+    #tacos_on_order - Taco(model)
     quantity_ordered = models.IntegerField()
     total_price = models.DecimalField(decimal_places=2, max_digits=5)
     ordered_by = models.ForeignKey(User, related_name='tacos_ordered', on_delete=models.CASCADE, default = None)
@@ -26,3 +18,52 @@ class Order(models.Model):
 
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True) 
+
+
+class Taco(models.Model):
+    #ingredients - Ingredient(model)
+    tortilla_choices = [
+        ('flour'),
+        ('corn'),
+    ]
+    tortilla = models.CharField(max_length = 10, choices = tortilla_choices, default = 'flour')
+    taco_price = models.DecimalField(decimal_places=2, max_digits=5)
+
+    order = models.ForeignKey(Order, related_name='tacos_on_order', on_delete=models.CASCADE)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+
+class Ingredient(models.Model):
+    ingredient_choices = [
+        ('egg'),
+        ('potato'),
+        ('bean'),
+        ('cheese'),
+        ('carne asada'),
+        ('chicken fajita'),
+        ('beef fajita'),
+        ('pastor'),
+        ('barbacoa'),
+        ('lengua'),
+        ('bacon'),
+        ('nopales'),
+        ('sausage'),
+    ]
+
+    ingredient = models.CharField(max_length = 255, choices = ingredient_choices)
+    ingredient_price = models.DecimalField(decimal_places=2, max_digits=5)
+    taco = models.ForeignKey(Taco, related_name='ingredients', on_delete=models.CASCADE)
+
+    created_at = models.DateTimeField(auto_now_add = True)
+    updated_at = models.DateTimeField(auto_now = True)
+
+    def __str__(self):
+        return self.ingredient + ' $' + self.ingredient_price
+    
+
+
+
+
